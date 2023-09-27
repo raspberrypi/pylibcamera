7f9c86f467e25ce39db593574ef5c43bb237b1f8
William Vinnicombe <william.vinnicombe@raspberrypi.com>
2023-09-27 17:48:36 +0100

    Update libcamera clone script

--- clone-libcamera.sh
+++ clone-libcamera.sh
@@ -2,15 +2,10 @@
 
 rm -rf libcamera
 
-# mkdir libcamera
-git clone --depth 1 git@github.com:raspberrypi/libcamera.git libcamera
+git clone git@github.com:raspberrypi/libcamera.git libcamera
 pushd libcamera
 
-# git init
-# git remote add -f origin git@github.com:raspberrypi/libcamera-pi5.git
-# git config core.sparsecheckout true
-# cp ../sparse-checkout .git/info
-# git checkout origin/main
+git checkout release-v0.0.5+83-bde9b04f
 git apply ../pypatch.patch
 
 popd
